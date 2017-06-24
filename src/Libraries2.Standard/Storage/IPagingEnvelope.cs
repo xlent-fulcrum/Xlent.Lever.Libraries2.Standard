4a9b09198746eca8129ac03000b7b4bbe03c536f
Lars Lindberg <lars.lindberg@xlent.se>
2017-06-24 09:57:47 +0200

    Added interface for paging envelope.

---
+++ src/Libraries2.Standard/Storage/IPagingEnvelope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xlent.Lever.Libraries2.Standard.Storage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IPagingEnvelope<TData, TId>
+        where TData : IStorable<TId>
+    {
+        /// <summary>
+        /// The data in this segment, this "page"
+        /// </summary>
+        IEnumerable<TData> Data { get; set; }
+
+        /// <summary>
+        /// Information about this segment of the data
+        /// </summary>
+        PageInfo PageInfo { get; set; }
+    }
+}
